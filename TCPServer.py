1fb15970b55f6f0b34dbb0191cbbbc6964d677a4
Nathan Hernandez <email@nathanph.com>
2017-05-15 10:22:48 -0400

    Update with some changes I made forever ago
    I have no idea what those changes are... Whoops.

--- TCPServer.py
+++ TCPServer.py
@@ -6,9 +6,12 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(5)
 
+# Send uppercased strings back to client.
 def uppercase(connection_socket):
     while True:
         sentence = connection_socket.recv(1024)
+        if not sentence:
+            break
         capitalized_sentence = sentence.upper()
         connection_socket.send(capitalized_sentence)
     connection_socket.close()
@@ -19,12 +22,4 @@ while True:
     # Thread here.
     thread = Thread(target=uppercase, args=(connectionSocket,))
     thread.start()
-
-
-# while True:
-#     sentence = connectionSocket.recv(1024)
-#     capitalizedSentence = sentence.upper()
-#     connectionSocket.send(capitalizedSentence)
-# connectionSocket.close()
-
 
